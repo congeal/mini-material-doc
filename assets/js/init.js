e5dbb896902382f192ff88389f5df84409fdb184
Eung Sup Kim <congeal@me.com>
2021-06-11 23:12:02 +0900

    TOC 기능 추가

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,5 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const M = window.M;
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+// M.AutoInit();
+$(document).ready(function () {
+    //Side navigation
+    $('.sidenav').sidenav();
+    
+    //TOC
+    if ($('.table-of-contents').size() != 0) {
+        let toc_l = $(".page_content h2").addClass('scrollspy')
+        .map(function(_, header) {
+            return "<li><a href='#" + header.id + "'>" + header.innerHTML + "</a></li>"
+        });
+        $(".table-of-contents").html(toc_l.toArray().join('\n'))
+        $('.scrollspy').scrollSpy();
+        $('.row .table-of-contents').pushpin({ top: $('.row').offset().top, offset: 75 });
+    }
 });
---
+++ page.md
@@ -0,0 +1,23 @@
+---
+layout: page
+---
+
+# Page layout
+## First
+* A
+* B
+* C
+
+## Second
+1. A
+2. B
+3. C
+
+## Third
+```verilog
+module something();
+endmodule
+```
+
+## Fourth
+something
