6e2e52ff17c5fc93648eca92312cc742ce072227
Eung Sup Kim <congeal@me.com>
2021-06-12 17:52:00 +0900

    Collections을 sidenav에 표시

---
+++ _docs/actions.md
@@ -0,0 +1,25 @@
+---
+layout: page
+---
+# Github docs: Actions
+## Automation, CI/CD
+## 정책
+### 권한
+### Pool 종류에 따른 Runner 선택
+## 시작하기
+### Actions 사용 신청
+Github support에서 Actions 사용 신청을 해주십시오. Actions 권한은 당분간 organization에만 허용합니다.
+### VWP 권한 신청
+LSF (*_sub)를 사용하려면 
+### Workflow 작성 예시
+* CDSP org에 runner 할당
+* CDSP/actions_test에 CDSP 설치
+* CDSP/actions_test에 lint RTL 예시를 올림
+	* master에는 CDSP lint pass하는 RTL을 올림
+	* new branch에 CDSP lint fail 되는 RTL을 올림
+* Actions 결과를 보여줌
+## 부가 기능
+### Badge
+* CDSP 예에서 Badge를 표시하는 것을 보여줌
+### Custom workflow
+* 외부에 이것을 설명하는 페이지를 링크
--- assets/js/init.js
+++ assets/js/init.js
@@ -2,13 +2,18 @@
 $(document).ready(function () {
     //Side navigation
     $('.sidenav').sidenav();
-    
+    $('.sidenav a').click(function (e) {
+        e.preventDefault();
+        var linkUrl = $(this).attr('href');
+        setTimeout(function (url) { window.location = url; }, 300, linkUrl);
+    });
+
     //TOC
     if ($('.table-of-contents').size() != 0) {
         let toc_l = $(".page_content h2").addClass('scrollspy')
-        .map(function(_, header) {
-            return "<li><a href='#" + header.id + "'>" + header.innerHTML + "</a></li>"
-        });
+            .map(function (_, header) {
+                return "<li><a href='#" + header.id + "'>" + header.innerHTML + "</a></li>"
+            });
         $(".table-of-contents").html(toc_l.toArray().join('\n'))
         $('.scrollspy').scrollSpy();
         $('.row .table-of-contents').pushpin({ top: $('.row').offset().top, offset: 75 });
